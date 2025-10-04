2f4951575a4bad2e99fb2e5b824c52d00a666ee2
derynm <dery02.nm@gmail.com>
2025-10-04 15:19:48 +0700

    initial project

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-indigo-800 text-white text-5xl flex justify-center items-center h-screen">
+    Sup!!
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
