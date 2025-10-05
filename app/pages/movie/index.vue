d88447be0d75d1a13fd2ebaefacaaac39654cba8
derynm <dery02.nm@gmail.com>
2025-10-05 18:00:31 +0700

    fix: fetcher at movie page

--- app/pages/movie/index.vue
+++ app/pages/movie/index.vue
@@ -33,7 +33,7 @@ const movies = ref<Movie[]>([])
 const route = useRoute()
 const router = useRouter()
 
-const { data: movieList } = await useFetchApi<MovieList>('discover/movie', {
+const { data: movieList } = useFetchApi<MovieList>('discover/movie', {
   query: {
     with_genres: route.query.genre,
     page
