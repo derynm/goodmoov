4fe6b408017e61ee0adcf9b1c64318b32fb98fc9
derynm <dery02.nm@gmail.com>
2025-10-05 22:31:54 +0700

    feat: change font using nuxt/fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,26 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts'],
   css: ['~/assets/css/main.css'],
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com',
-          crossorigin: ''
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
-        }
-      ]
-    }
+  fonts: {
+    families: [
+      {
+        name: 'Poppins',
+        provider: 'google',
+        weights: [300, 400, 500, 600, 700],
+        display: 'swap'
+      }
+    ]
   },
   vite: {
     plugins: [tailwindcss()]
